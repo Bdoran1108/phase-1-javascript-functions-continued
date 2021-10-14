e8e82f04ff07b94fbafecbe1e628034a9cde125e
Brian <Briandoran1@icloud.com>
2021-10-14 00:45:46 -0400

    commit tree 57873e64768c0b3c67acefa452e7893c88624ef6

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code here
+function saturdayFun (activity) {
+
+return ("This Saturday, I want to ${activity}!");
+
+if (activity === "" ) return ("This Saturday, I want to roller-skate");
+
+}
